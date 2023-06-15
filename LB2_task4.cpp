0c8d7c50010a29d0580904600b318b95da2d4ce9
YeaNipul <136602852+YeaNipul@users.noreply.github.com>
2023-06-15 22:38:43 +0600

    Add files via upload

---
+++ LB2_task4.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int array[10];
+    cout<<"Array_1=";
+    for(int i=0; i<10; i++)
+{
+    cin>>array[i];
+}
+int count;
+int i;
+for(count=array[i]; count<=array[i]; count++)
+{
+    if(count==array[i])
+   {
+    count++;
+  }
+}
+cout<<"Number occurs"<<"   "<< count<<"times";
+
+return 0;
+}
