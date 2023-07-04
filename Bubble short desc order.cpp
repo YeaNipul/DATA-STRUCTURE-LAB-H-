ec20469a104c8eb8ccf5f7e1bfebbac96b788388
YeaNipul <136602852+YeaNipul@users.noreply.github.com>
2023-07-04 22:23:30 +0600

    Add files via upload

---
+++ Bubble short desc order.cpp
@@ -0,0 +1,36 @@
+//Bubble short descending order
+#include <iostream>
+using namespace std;
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (arr[j] < arr[j + 1]) {
+                
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {64, 48, 69, 36, 46, 90, 111};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Original array: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    bubbleSort(arr, size);
+
+    std::cout << "Sorted array (descending order): ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
